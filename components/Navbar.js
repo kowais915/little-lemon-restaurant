cea1c5e0114cea2248a825370da3532b1d5c7fc4
kowais915 <kowais915@gmail.com>
2023-06-04 21:10:11 +0500

    :white_check_mark: Done with the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,34 @@
+import styles from '@/styles/Navbar.module.css'
+import Link from 'next/link'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
+
+
+
 const Navbar = () => {
     return (  
-        <nav className="navbar">
-            <h1>This is the navbar</h1>
+        <nav className={styles.container}>
+            <div className={styles.brand}>
+                <h1>
+                    Little Lemon
+                </h1>
+            </div>
+
+            <div className={styles.links}>
+
+                <Link href="/" className={styles.navlink}>Home</Link>
+                <Link href="/about" className={styles.navlink}>About</Link>
+                <Link href="/contact" className={styles.navlink}>Book</Link>
+                <Link href="/blog" className={styles.navlink}>Contact</Link>
+                
+               <Link href="/">
+               <FontAwesomeIcon icon={faShoppingCart}  className={styles.icon}/>
+                </Link> 
+               
+                
+            </div>
         </nav>
     );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import Layout from '@/components/Layout'
 import '@/styles/globals.css'
+ // import Font Awesome CSS
+
+ import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+ import { config } from "@fortawesome/fontawesome-svg-core";
+ config.autoAddCss = false;
+
 
 export default function App({ Component, pageProps }) {
 
@@ -7,7 +13,7 @@ export default function App({ Component, pageProps }) {
     <Layout>
 
         <Component {...pageProps} />
-        
+
     </Layout>
 
   )
