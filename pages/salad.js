8ccc954fa7e2c86762ca320005b9646773017957
kowais915 <kowais915@gmail.com>
2023-06-05 02:08:29 +0500

    :white_check_mark: Done with the salad page

--- pages/salad.js
+++ pages/salad.js
@@ -1,6 +1,18 @@
 import Hero from "@/components/Hero";
+import Item from "@/components/Item";
 
 const Salad = () => {
+
+   const salad = [
+         {name: "Caesar", price: 10, description: "The best caesar salad in town", picture: "/salads.png"},
+         {name: "Greek", price: 10, description: "The best greek salad in town", picture: "/salads.png"},
+         {name: "Cobb", price: 10, description: "The best cobb salad in town", picture: "/salads.png"},
+         {name: "Waldorf", price: 10, description: "The best waldorf salad in town", picture: "/salads.png"},
+         {name: "Pork", price: 10, description: "The best pork salad in town", picture: "/salads.png"},
+         {name: "Beef", price: 10, description: "The best beef salad in town", picture: "/salads.png"},
+         {name: "Chicken", price: 10, description: "The best chicken salad in town", picture: "/salads.png"},
+         {name: "Tuna", price: 10, description: "The best tuna salad in town", picture: "/salads.png"},
+   ]
     return ( 
         <>
            <Hero
@@ -8,6 +20,23 @@ const Salad = () => {
                 motto="The best salad in town"
                 picture="/salads.png"
            />
+
+           <div className="pizzaGrid">
+
+               {salad.map((salad) => {
+                  return(
+                     <Item
+                        key={salad.name}
+                        name={salad.name}
+                        price={salad.price}
+                        description={salad.description}
+                        picture={salad.picture}
+                        
+
+                     />
+                  )
+               })}
+           </div>
         
         </>
      );
