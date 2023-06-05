48d97ed811c2be015be7394ce33d47129fce395d
kowais915 <kowais915@gmail.com>
2023-06-05 20:42:10 +0500

    :sparkles: Added the cart empty/unempty logic

--- pages/cart.js
+++ pages/cart.js
@@ -1,12 +1,23 @@
 import Hero from "@/components/Hero";
+import { useSelector } from 'react-redux';
 
 const Cart = () => {
+    const {cart} = useSelector(state => state.cart);
+    let motto = '';
+
+    if (cart == 0){
+        motto = 'Your cart is empty'
+    }else{
+        motto = 'Here are all your items.'
+    }
+
     return (  
         <>
+
             <Hero
 
                 title="Cart"
-                motto="Here is all your items"
+                motto = {motto}
                 picture= "/finalCart.png"
             />
             
